61d4bd04739e833f796bf6673f9926703875397f
Max Afonov <max@bumnetworks.com>
2010-10-21 05:41:03 -0400

    added some comments describing what is wrong

--- trauma-core/src/main/scala/C.scala
+++ trauma-core/src/main/scala/C.scala
@@ -1,8 +1,10 @@
 package trauma.core
 
+// these two imports don't type-check
 import trauma.util.A
 import trauma.model.B
 
 object C {
+  // A.<TAB> and B.<TAB> don't show A.a and B.b as candidates for completion
   val c = "%s + %s + c".format(A.a, B.b)
 }
--- trauma-model/src/main/scala/B.scala
+++ trauma-model/src/main/scala/B.scala
@@ -1,7 +1,9 @@
 package trauma.model
 
+// this import doesn't type-check
 import trauma.util.A
 
 object B {
+  // A.<TAB> doesn't show A.a as a candidate for completion
   val b = "%s + b".format(A.a)
 }
